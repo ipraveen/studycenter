9bd4121e117c9bc2a6bedc69d3a9ee62f649106c
psingh7 <praveenkumar_singh@intuit.com>
2018-07-02 01:28:05 +0530

    code cleanup.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HelloWorld from "./components/HelloWorld";
 
- //eslint-disable-next-line no-undef
-if (process.env.NODE_ENV !== 'production') {
-
-    //eslint-disable-next-line no-console
-    console.log('Looks like we are in development mode!');
-}
 ReactDOM.render(<HelloWorld />, document.getElementById("app"));
 
 
